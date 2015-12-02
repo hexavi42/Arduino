da9bf298b26e40c3093030d1e4820ca574a02926
Harvey Tang <harvey.xw.tang@gmail.com>
2015-12-01 23:49:56 -0500

    Directory change for Arduino IDE

---
+++ serialText/serialText.ino
@@ -0,0 +1,99 @@
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include "RGB.h"
+
+#define PIN 1
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800);
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+
+  // LED matrix setup
+  matrix.begin();
+  matrix.setBrightness(20);
+  matrix.setTextColor( matrix.Color(white.r, white.g, white.b) );
+  matrix.setTextWrap(false);
+  
+}
+
+void loop() {
+  serialEvent(); //call the function
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    scrollText(inputString)
+    // clear the string:
+inputString = "";
+stringComplete = false;
+}
+
+/*
+SerialEvent occurs whenever a new data comes in the
+hardware serial RX. This routine is run between each
+time loop() runs, so using delay inside loop can delay
+response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+stringComplete = true;
+    }
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(RGB color, uint8_t wait) {
+  for(uint16_t row=0; row < 8; row++) {
+    for(uint16_t column=0; column < 8; column++) {
+      matrix.drawPixel(column, row, matrix.Color(color.r, color.g, color.b));
+      matrix.show();
+      delay(wait);
+    }
+  }
+}
+
+void drawFast(byte pic[8][8][3]) {
+  for(int row = 0; row < 8; row++) {
+    for(int column = 0; column < 8; column++) {
+      matrix.drawPixel(column, row, matrix.Color(pic[column][row][0], pic[column][row][1], pic[column][row][2]));
+      matrix.show();
+    }
+  }
+}
+
+void scrollText(String textToDisplay) {
+  int x = matrix.width();
+  int pixelsInText = textToDisplay.length() * 7;
+  
+  matrix.setCursor(x, 0);
+  matrix.print(textToDisplay);
+  matrix.show();
+  
+  while(x > (matrix.width() - pixelsInText)) {
+    matrix.fillScreen(matrix.Color(teal.r, teal.g, teal.b));
+    matrix.setCursor(--x, 0);
+    matrix.print(textToDisplay);
+    matrix.show();
+    delay(150);
+  }
+}
+
+
+
+
